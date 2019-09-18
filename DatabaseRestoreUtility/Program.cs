79e95f5399589696fee2cf63996f6f95293e0956
billshu <bill.shu@gravitysupplychain.com>
2019-09-18 11:35:17 +0800

    Make sure that restore bak file will work

--- DatabaseRestoreUtility/Program.cs
+++ DatabaseRestoreUtility/Program.cs
@@ -16,13 +16,18 @@ namespace DatabaseRestoreUtility
             var bakFilePath = args.Length > 2 ?
                 args[2] : throw new ArgumentException($"Please pass .bak file path as the third argument");
 
+            var logicalDbName = args.Length > 3?
+                args[3] : throw new ArgumentException($"Please pass the database name in the .bak file");
+            var dataDir = args.Length > 4? 
+                args[4] : throw new ArgumentException($"Please pass the data directory which will store the restored database");
+
             var fi = new FileInfo(bakFilePath);
             if (!fi.Exists)
             {
                 throw new ArgumentException($"The provided .bak file path {bakFilePath} doesn't exist");
             }
 
-            new RestoreService().RestoreDatabase(connectionString, database, bakFilePath);
+            new RestoreService().RestoreDatabase(connectionString, database, bakFilePath, logicalDbName, dataDir);
         }
     }
 }
--- DatabaseRestoreUtility/RestoreService.cs
+++ DatabaseRestoreUtility/RestoreService.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace DatabaseRestoreUtility
 {
     public class RestoreService
     {
         private static readonly int DefaultCommandTimeout = (int)TimeSpan.FromMinutes(20).TotalSeconds;
-        public void RestoreDatabase(string connectionString, string databaseName, string bakFilePath, int? timeout = default)
+        public void RestoreDatabase(string connectionString, 
+            string databaseName, 
+            string bakFilePath, 
+            string logicalDbName,
+            string dataDir,
+            int? timeout = default)
         {
             using (var connection = new SqlConnection(connectionString))
             {
-                var query = $"RESTORE DATABASE {databaseName} FROM DISK='{bakFilePath}'";
+                var query = $@"RESTORE DATABASE {databaseName} 
+FROM DISK='{bakFilePath}'
+WITH MOVE '{logicalDbName}' TO '{Path.Combine(dataDir, logicalDbName + ".mdf")}',
+MOVE '{logicalDbName}_log' TO '{Path.Combine(dataDir, logicalDbName + ".ldf")}'
+";
                 using (var command = new SqlCommand(query, connection)
                 {
                     CommandTimeout = timeout ?? DefaultCommandTimeout
