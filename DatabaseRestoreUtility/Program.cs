a10a80a3d0928febd7434a102b707fb7f266e737
billshu <bill.shu@gravitysupplychain.com>
2019-12-08 07:28:25 +0800

    Add timeout parameter

--- DatabaseRestoreUtility/Program.cs
+++ DatabaseRestoreUtility/Program.cs
@@ -20,6 +20,21 @@ namespace DatabaseRestoreUtility
             var dbName = args.Length > 3? 
                 args[3] : throw new ArgumentException($"Please pass the name of destination database"); 
 
+            var timeoutString = args.Length > 4?
+                args[4] : null;
+
+            if (timeoutString != null && 
+                (!int.TryParse(timeoutString, out var timeout) ||
+                 timeout <= 0)
+                )
+            {
+                throw new ArgumentException($"The passed timeout is not a valid integer");
+            }
+            else
+            {
+                timeout = 0;
+            }
+
             var fi = new FileInfo(bakFilePath);
             if (!fi.Exists)
             {
@@ -35,7 +50,8 @@ namespace DatabaseRestoreUtility
             await RestoreService.RestoreDatabase(connectionString, 
                 bakFilePath, 
                 dataDir, 
-                dbName);
+                dbName,
+                timeout > 0? timeout : (int?)null);
             Console.WriteLine($"Finished restoring bak file {bakFilePath} to database {dbName}");
         }
     }
